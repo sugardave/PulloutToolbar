32ae941f639895f3bf43cedd0c59bbb5de425fe9
Dave Freeman <sugardave@gmail.com>
2012-07-10 23:33:25 -0500

    poorly documented initial commit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 PulloutToolbar
-==============
-
+===
 A simple PulloutToolbar based on enyo.Popup
+
+Requires https://github.com/enyojs/layout and https://github.com/enyojs/onyx
+
+Known Issues
+===
+Pulltab position "center" isn't really centered.  It uses percentages to position offset from left or top and can look "not-centered" when resizing to smaller viewports.
---
+++ source/PulloutToolbar.js
@@ -0,0 +1,179 @@
+enyo.kind({
+	name: "PulloutToolbar",
+	kind: enyo.Popup,
+	floating: true,
+	centered: false,
+	modal: false,
+	edge: "left",
+	classes: "pullout-toolbar",
+	published: {
+		autoCollapse: true,
+		pulltabPosition: -1 // -1 = left/top, 0 = center (pretty close), 1 = right/bottom
+	},
+	create: function() {
+		this.inherited(arguments);
+		this.pulltabPositionChanged();
+	},
+	initComponents: function() {
+		var edges = ["top", "right", "bottom", "left"], edge;
+		for (var e in edges) {
+			edge = edges[e];
+			this.removeClass(edge);
+		}
+		
+		this.removeClass("horizontal");
+		this.removeClass("vertical");
+		
+		this.createClient();
+		
+		this.addClass(this.edge);
+		
+		switch(this.edge) {
+			case "top":
+			case "bottom":
+				this.$.clientWrapper.setLayoutKind(enyo.FittableColumnsLayout);
+				this.addClass("horizontal");
+				this.$.slider.setLayoutKind(enyo.FittableRowsLayout);
+				this.$.client.setLayoutKind(enyo.FittableColumnsLayout);
+				break;
+			case "right":
+			case "left":
+				this.$.clientWrapper.setLayoutKind(enyo.FittableRowsLayout);
+				this.addClass("vertical");
+				this.$.slider.setLayoutKind(enyo.FittableColumnsLayout);
+				this.$.client.setLayoutKind(enyo.FittableRowsLayout);
+				break;
+		}
+		this.inherited(arguments);
+	},
+	rendered: function() {
+		this.inherited(arguments);
+		// need to let the DOM nodes render before setting the slider values
+		setTimeout(enyo.bind(this, "setSlider", 0));
+	},
+	pulltabPositionChanged: function() {
+		var positions = ["left", "top", "center", "right", "bottom"], position;
+		var orientation = (this.edge === "left" || this.edge === "right") ? "vertical" : "horizontal";
+		for (var p in positions) {
+			position = positions[p];
+			this.$.pulltab.removeClass(position);
+		}
+		switch(this.pulltabPosition) {
+			case -1:
+				position = (orientation === "vertical") ? "top" : "left";
+				break;
+			case 0:
+				position = "center";
+				break;
+			case 1:
+				position = (orientation === "vertical") ? "bottom" : "right";
+				break;
+		}
+		this.$.pulltab.addClass(position);
+	},
+	createClient: function() {
+		var components = [];
+		this.createComponent(
+			{name: "slider", kind: enyo.Slideable, overMoving: false, onChange: "checkState", classes: "slider"}
+		);
+		switch(this.edge) {
+			case "top":
+			case "left":
+				components = [
+					{name: "clientWrapper", components: [
+						{name: "client", fit: true, classes: "onyx-toolbar onyx-toolbar-inline"}
+					]},
+					{name: "pulltab", kind: enyo.Control, classes: "onyx-toolbar pulltab", components: [
+						{kind: "onyx.Grabber", classes: "grabber"}
+					]}
+				];
+				break;
+			case "bottom":
+			case "right":
+				components = [
+					{name: "pulltab", kind: enyo.Control, classes: "onyx-toolbar pulltab", components: [
+						{kind: "onyx.Grabber", classes: "grabber"}
+					]},
+					{name: "clientWrapper", components: [
+						{name: "client", fit: true, classes: "onyx-toolbar onyx-toolbar-inline"}
+					]}
+				];
+				break;
+		}
+		this.$.slider.createComponents(components, {owner: this});
+	},
+	hide: function() {
+		if (this.autoCollapse) {
+			(this.hasClass("left") || this.hasClass("top")) ? this.$.slider.animateToMin() : this.$.slider.animateToMax();
+		}
+	},
+	show: function() {
+		this.inherited(arguments);
+		(this.hasClass("left") || this.hasClass("top")) ? this.$.slider.animateToMax() : this.$.slider.animateToMin();
+	},
+	setSlider: function() {
+		var popup = this;
+		var popupNode = popup.hasNode();
+		var popupWidth = popupNode.clientWidth;
+		var popupHeight = popupNode.clientHeight;
+		
+		var slider = this.$.slider;
+		var sliderNode = slider.hasNode();
+		var sliderWidth = sliderNode.clientWidth;
+		var sliderHeight = sliderNode.clientHeight;
+		var axis, unit, min, max, value;
+		switch(this.edge) {
+			case "top":
+				axis = "v";
+				unit = "px";
+				max = 0;
+				min = -sliderHeight;
+				break;
+			case "right":
+				axis = "h";
+				unit = "%";
+				max = 100;
+				min = 100 - (Math.round(sliderWidth / popupWidth * 100));
+				min = 0;
+				break;
+			case "bottom":
+				axis = "v";
+				unit = "%";
+				max = 100;
+				min = 100 - (Math.round(sliderHeight / popupHeight * 100));
+				min = 0;
+				break;
+			case "left":
+				axis = "h";
+				unit = "px";
+				max = 0;
+				min = -sliderWidth;
+				break;
+		}
+		
+		slider.setAxis(axis);
+		slider.setUnit(unit);
+		slider.setMin(min);
+		slider.setMax(max);
+	},
+	checkState: function() {
+		switch(this.edge) {
+			case "top":
+			case "left":
+				if (this.$.slider.isAtMax()) {
+					this.removeClass("collapsed");
+				} else if (this.$.slider.isAtMin()) {
+					this.addClass("collapsed");
+				}
+				break;
+			case "bottom":
+			case "right":
+				if (this.$.slider.isAtMin()) {
+					this.removeClass("collapsed");
+				} else if (this.$.slider.isAtMax()) {
+					this.addClass("collapsed");
+				}
+				break;
+		}
+	}
+});
